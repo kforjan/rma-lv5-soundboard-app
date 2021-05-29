061415094bbb41e4e1c07c5e9327d5b6da713720
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-29 23:33:22 +0200

    Add image assets

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -7,11 +7,55 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: GridView(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+      body: Center(
+        child: GridView(
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {},
+                child: Image.asset(
+                  'assets/images/dog.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {},
+                child: Image.asset(
+                  'assets/images/cat.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {},
+                child: Image.asset(
+                  'assets/images/duck.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {},
+                child: Image.asset(
+                  'assets/images/cow.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
         ),
-        children: [],
       ),
     );
   }
